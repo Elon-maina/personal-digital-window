5b1cf28a8a382948d1441704a5d554c3d2e5144a
Elon-maina <branhamzdiaz@gmail.com>
2025-05-23 13:07:23 +0300

    add all new changes

--- server.cjs
+++ server.cjs
@@ -14,7 +14,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
+  password: process.env.DB_PASSWORD || '@$1A2b3c4d',
   database: 'feedback'
 });
 
